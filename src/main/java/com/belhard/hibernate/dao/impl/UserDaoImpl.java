0755efe673697b3118632d2151ae2ade4d0ac04a
Anastasiya Oleshko <anastasiya.oleshko13@gmail.com>
2023-01-03 19:42:54 +0300

    Created UserDaoImpl

---
+++ src/main/java/com/belhard/hibernate/dao/impl/UserDaoImpl.java
@@ -0,0 +1,33 @@
+package com.belhard.hibernate.dao.impl;
+
+import com.belhard.hibernate.dao.UserDao;
+import com.belhard.hibernate.entity.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User find(Long key) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User create(User entity) {
+        return null;
+    }
+
+    @Override
+    public User update(User entity) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long key) {
+        return false;
+    }
+}
